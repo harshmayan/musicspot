7471d77ccea2b154b38ea72c2bf01730d493c49c
harshmayan <harshmayani07@gmail.com>
2023-08-30 15:52:29 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "hyegpswnvfnxgadscgsi.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
