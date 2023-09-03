8acbf680fcb23090297b9e10976f8eaedee70fec
harshmayan <harshmayani07@gmail.com>
2023-09-03 16:15:22 +0530

    Heading changed in library

--- app/mobilelibrary/components/LibraryContent.tsx
+++ app/mobilelibrary/components/LibraryContent.tsx
@@ -45,7 +45,7 @@ const LibraryContent: React.FC<LibraryContentProps> = ({ songs }) => {
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist className="text-neutral-400" size={26} />
-          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+          <p className="text-neutral-400 font-medium text-md">My Library</p>
         </div>
         <AiOutlinePlus
           onClick={onClick}
--- app/mobilelibrary/page.tsx
+++ app/mobilelibrary/page.tsx
@@ -24,7 +24,7 @@ const Library= async () => {
             <Header className="from-bg-neutral-900">
                 <div className="mb-2 flex flex-col gap-y-6">
                     <h1 className="text-white text-3xl font-semibold">
-                        My PlayList 
+                        Add Your Favourite Songs
                     </h1>
                 </div>
             </Header>
